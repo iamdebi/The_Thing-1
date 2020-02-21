4544f28ef8488ec8faaf6f750b78e22858feec7a
Brendan <laotsu@hotmail.co.uk>
2020-02-21 13:21:05 +0000

    Server file structure started

---
+++ server/src/main/java/models/Player.java
@@ -0,0 +1,101 @@
+package models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "players")
+public class Player {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "party")
+    private String party;
+
+    @Column(name = "captain")
+    private Boolean captain;
+
+    @Column(name = "coCaptain")
+    private Boolean coCaptain;
+
+    @Column()
+    private String vote;
+
+    @Column(name = "cards")
+    private List<Card> cards;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Player() {
+    }
+
+    public Player(String name) {
+        this.name = name;
+        this.role = "";
+        this.party = "";
+        this.captain = false;
+        this.coCaptain = false;
+        this.vote = "";
+        this.cards = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    String getParty() {
+        return party;
+    }
+
+    void setParty(String party) {
+        this.party = party;
+    }
+
+    Boolean getCaptain() {
+        return captain;
+    }
+
+    void setCaptain(Boolean captain) {
+        this.captain = captain;
+    }
+
+    Boolean getCoCaptain() {
+        return coCaptain;
+    }
+
+    void setCoCaptain(Boolean coCaptain) {
+        this.coCaptain = coCaptain;
+    }
+
+    String getVote() {
+        return vote;
+    }
+
+    void setVote(String vote) {
+        this.vote = vote;
+    }
+
+    Long getId() {
+        return id;
+    }
+
+}
---
+++ server/src/test/java/com/example/The_Thing/TheThingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.The_Thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheThingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
