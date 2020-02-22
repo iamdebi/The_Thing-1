db5ae033a7986de87c108d986b02b62beb8b968d
Brendan <laotsu@hotmail.co.uk>
2020-02-22 13:05:11 +0000

    Player controller startrd

--- server/src/main/java/com/example/The_Thing/controllers/PlayerController.java
+++ server/src/main/java/com/example/The_Thing/controllers/PlayerController.java
@@ -1,7 +1,33 @@
 package com.example.The_Thing.controllers;
 
+import com.example.The_Thing.models.Player;
+import com.example.The_Thing.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PlayerController {
+
+    @Autowired
+    private PlayerRepository playersRepository;
+
+    @GetMapping(value = "/players")
+    ResponseEntity<List<Player>> findAllPlayers() {
+        return new ResponseEntity<>(playersRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/players/{id}")
+    ResponseEntity findPlayer(@PathVariable Long id) {
+        return new ResponseEntity<>(playersRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value =)
+
 }
--- server/src/main/java/com/example/The_Thing/repositories/GameRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.The_Thing.repositories;
-import com.example.The_Thing.models.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-}
