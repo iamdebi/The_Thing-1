a85ad8f4e7d0ee707339a7d9e30782b82bc960d7
Brendan <laotsu@hotmail.co.uk>
2020-02-25 18:37:23 +0000

    Get routes for board added

--- server/src/main/java/com/example/The_Thing/controllers/ImitationBlockController.java
+++ server/src/main/java/com/example/The_Thing/controllers/ImitationBlockController.java
@@ -1,17 +1,25 @@
 package com.example.The_Thing.controllers;
 
 import com.example.The_Thing.models.ImitationBlocked;
+import com.example.The_Thing.models.Player;
 import com.example.The_Thing.repositories.ImitationBlockedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class ImitationBlockController {
 
         @Autowired
         ImitationBlockedRepository imitationBlockRepository;
+
+        @GetMapping(value = "/players")
+            ResponseEntity<List<ImitationBlocked>> findAllImitationBlocks() {
+        return new ResponseEntity<>(imitationBlockRepository.findAll(), HttpStatus.OK);
+        }
 
         @PatchMapping(value = "/imitation-block/{id}")
         ResponseEntity<ImitationBlocked> updateImitationBlock(@RequestBody ImitationBlocked imitationBlock, @PathVariable Long id) {
--- server/src/main/java/com/example/The_Thing/controllers/ImitationPassController.java
+++ server/src/main/java/com/example/The_Thing/controllers/ImitationPassController.java
@@ -1,5 +1,6 @@
 package com.example.The_Thing.controllers;
 
+import com.example.The_Thing.models.ImitationBlocked;
 import com.example.The_Thing.models.ImitationPassed;
 import com.example.The_Thing.repositories.ImitationPassedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class ImitationPassController {
 
     @Autowired
     ImitationPassedRepository imitationPassRepository;
 
+    @GetMapping(value = "/players")
+    ResponseEntity<List<ImitationPassed>> findAllImitationPassed() {
+        return new ResponseEntity<>(imitationPassRepository.findAll(), HttpStatus.OK);
+    }
+
     @PatchMapping(value = "/imitation-pass/{id}")
-    ResponseEntity<ImitationPassed> updateImitationPass(@RequestBody ImitationPassed imitationPass, @PathVariable Long id) {
-        imitationPassRepository.save(imitationPass);
-        return new ResponseEntity<ImitationPassed>(imitationPass, HttpStatus.CREATED);
+    ResponseEntity<ImitationPassed> updateImitationPass(@RequestBody ImitationPassed imitationPassed, @PathVariable Long id) {
+        imitationPassRepository.save(imitationPassed);
+        return new ResponseEntity<ImitationPassed>(imitationPassed, HttpStatus.CREATED);
     }
 
     @DeleteMapping(value = "/imitation-passes")
