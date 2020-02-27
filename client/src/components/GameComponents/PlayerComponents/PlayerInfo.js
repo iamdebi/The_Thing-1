c7a8813edc9ffc97789042c3206cf154a036f411
Debbie Urquhart <debiurquhart@live.co.uk>
2020-02-27 09:27:07 +0000

    changes player cards color

--- client/src/components/GameComponents/PlayerComponents/PlayerInfo.js
+++ client/src/components/GameComponents/PlayerComponents/PlayerInfo.js
@@ -6,41 +6,63 @@ const PlayerDetail = props => {
     return "Loading ....";
   }
 
-  function populateAlienTeam(array){
-    for (var player of array){
-      if(player.party === "Aliens"){
-        props.aliens.push(player)
+  function populateAlienTeam(array) {
+    for (var player of array) {
+      if (player.party === "Aliens") {
+        props.aliens.push(player);
       }
     }
     return props.aliens;
   }
   populateAlienTeam(props.players);
 
-  function renderParty(){
-    if(props.player.party === "Aliens" ){
-      return props.player.party + ": " + props.aliens[0].name + " - " + props.aliens[0].role + " & " + props.aliens[1].name + " - " + props.aliens[1].role;
-    } else if
-      (props.player.party ==="Humans"){
+  function renderParty() {
+    if (props.player.party === "Aliens") {
+      return (
+        props.player.party +
+        ": " +
+        props.aliens[0].name +
+        " - " +
+        props.aliens[0].role +
+        " & " +
+        props.aliens[1].name +
+        " - " +
+        props.aliens[1].role
+      );
+    } else if (props.player.party === "Humans") {
       return props.player.party;
-      }
     }
+  }
 
+  let playerType;
+
+  if (props.player.party === "Humans") {
+    playerType = "passedImitation";
+  } else {
+    playerType = "blockedImitation";
+  }
 
   return (
-      <div className="player">
-        <h2><u>{props.player.name}</u></h2>
-        <p>Keep this information secret.</p>
-        <p><u>Role:</u></p>
-        <p>{props.player.role}</p>
-        <p><u>Party:</u></p>
-        <div>
-          <p>{renderParty()}</p>
-        </div>
-        <Link to="/thething/players">
-          <button type="button">Back to All Players</button>
-        </Link>
+    <div className={playerType}>
+      <h2>
+        <u>{props.player.name}</u>
+      </h2>
+      <p>Keep this information secret.</p>
+      <p>
+        <u>Role:</u>
+      </p>
+      <p>{props.player.role}</p>
+      <p>
+        <u>Party:</u>
+      </p>
+      <div>
+        <p>{renderParty()}</p>
+      </div>
+      <Link to="/thething/players">
+        <button type="button">Back to All Players</button>
+      </Link>
     </div>
   );
-}
+};
 
 export default PlayerDetail;
