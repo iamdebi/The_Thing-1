97407b01b1a65ec0c87490ca662c4a4a36dc1c01
Debbie Urquhart <debiurquhart@live.co.uk>
2020-04-07 11:23:19 +0100

    seperates css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import MainContainer from "./containers/MainContainer.js"
+import React from "react";
+import "./StyleSheets/Main.css";
+import "./StyleSheets/PreGame.css";
+import "./StyleSheets/Role.css";
+import "./StyleSheets/Player.css";
+import "./StyleSheets/Card.css";
+import MainContainer from "./containers/MainContainer.js";
 
 function App() {
-  return (
-      <MainContainer />
-  );
+  return <MainContainer />;
 }
 
 export default App;
